c0776b6d654619a3d2c0e8b5b6e50222ffc5e106
Blue Eagle <44013704+SaherBlueEagle@users.noreply.github.com>
2026-07-07 17:36:47 +0300

    Add files via upload

---
+++ TCPMonitor/frmmain.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmmain))
+        Me.cmdRefresh = New System.Windows.Forms.Button()
+        Me.LstItems = New System.Windows.Forms.ListView()
+        Me.cFile = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cLocal = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cLocalPort = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cRemoteAddr = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cRemotePort = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cState = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cPID = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ImgList = New System.Windows.Forms.ImageList(Me.components)
+        Me.cmdKill = New System.Windows.Forms.Button()
+        Me.cmdClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdRefresh
+        '
+        Me.cmdRefresh.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdRefresh.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdRefresh.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.cmdRefresh.Location = New System.Drawing.Point(8, 288)
+        Me.cmdRefresh.Name = "cmdRefresh"
+        Me.cmdRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.cmdRefresh.TabIndex = 1
+        Me.cmdRefresh.Text = "&Refresh"
+        Me.cmdRefresh.UseVisualStyleBackColor = False
+        '
+        'LstItems
+        '
+        Me.LstItems.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LstItems.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.cFile, Me.cLocal, Me.cLocalPort, Me.cRemoteAddr, Me.cRemotePort, Me.cState, Me.cPID})
+        Me.LstItems.FullRowSelect = True
+        Me.LstItems.Location = New System.Drawing.Point(8, 8)
+        Me.LstItems.Name = "LstItems"
+        Me.LstItems.Size = New System.Drawing.Size(752, 272)
+        Me.LstItems.SmallImageList = Me.ImgList
+        Me.LstItems.TabIndex = 0
+        Me.LstItems.UseCompatibleStateImageBehavior = False
+        Me.LstItems.View = System.Windows.Forms.View.Details
+        '
+        'cFile
+        '
+        Me.cFile.Text = "Filename"
+        Me.cFile.Width = 100
+        '
+        'cLocal
+        '
+        Me.cLocal.Text = "Local Host"
+        Me.cLocal.Width = 120
+        '
+        'cLocalPort
+        '
+        Me.cLocalPort.Text = "Local Port"
+        '
+        'cRemoteAddr
+        '
+        Me.cRemoteAddr.Text = "Remote Host"
+        Me.cRemoteAddr.Width = 120
+        '
+        'cRemotePort
+        '
+        Me.cRemotePort.Text = "Remote Port"
+        Me.cRemotePort.Width = 80
+        '
+        'cState
+        '
+        Me.cState.Text = "Stats"
+        Me.cState.Width = 120
+        '
+        'cPID
+        '
+        Me.cPID.Text = "PID"
+        Me.cPID.Width = 80
+        '
+        'ImgList
+        '
+        Me.ImgList.ImageStream = CType(resources.GetObject("ImgList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImgList.TransparentColor = System.Drawing.Color.Fuchsia
+        Me.ImgList.Images.SetKeyName(0, "app.png")
+        '
+        'cmdKill
+        '
+        Me.cmdKill.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdKill.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdKill.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.cmdKill.Location = New System.Drawing.Point(88, 288)
+        Me.cmdKill.Name = "cmdKill"
+        Me.cmdKill.Size = New System.Drawing.Size(104, 23)
+        Me.cmdKill.TabIndex = 2
+        Me.cmdKill.Text = "Close &Process"
+        Me.cmdKill.UseVisualStyleBackColor = False
+        '
+        'cmdClose
+        '
+        Me.cmdClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.cmdClose.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.cmdClose.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.cmdClose.Location = New System.Drawing.Point(195, 288)
+        Me.cmdClose.Name = "cmdClose"
+        Me.cmdClose.Size = New System.Drawing.Size(64, 23)
+        Me.cmdClose.TabIndex = 3
+        Me.cmdClose.Text = "&Close"
+        Me.cmdClose.UseVisualStyleBackColor = False
+        '
+        'frmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(768, 320)
+        Me.Controls.Add(Me.cmdClose)
+        Me.Controls.Add(Me.cmdKill)
+        Me.Controls.Add(Me.LstItems)
+        Me.Controls.Add(Me.cmdRefresh)
+        Me.Name = "frmmain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TCP Monitor"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents cmdRefresh As System.Windows.Forms.Button
+    Friend WithEvents LstItems As System.Windows.Forms.ListView
+    Friend WithEvents cFile As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cLocal As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cLocalPort As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cRemoteAddr As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cRemotePort As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cState As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cPID As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cmdKill As System.Windows.Forms.Button
+    Friend WithEvents ImgList As System.Windows.Forms.ImageList
+    Friend WithEvents cmdClose As System.Windows.Forms.Button
+
+End Class
---
+++ TCPMonitor/frmmain.vb
@@ -0,0 +1,261 @@
+﻿Option Explicit On
+
+Public Class frmmain
+
+    Private Structure TcpInfo
+        Dim LocalAddr As String
+        Dim LocalPort As Integer
+        Dim RemoteAddr As String
+        Dim RemotePort As Integer
+        Dim State As String
+        Dim PID As Integer
+        Dim Data As String
+    End Structure
+
+    Private TCPConnections() As TcpInfo
+    Private SelectedPID As Long = 0
+
+    Private Sub RefreshAll()
+        'Capture dos output of netstat
+        Dim Buff As String = CaptureDos("netstat", "-no")
+
+        If String.IsNullOrEmpty(Buff) Then
+            SetError("Cannot receive data.")
+        Else
+            'Clear list view.
+            LstItems.Items.Clear()
+            'Process buffer string.
+            ProcessData(Buff)
+            'Show connectiuons.
+            ShowTCPConnections()
+        End If
+    End Sub
+
+    Private Sub ProcessData(ByVal Source As String)
+
+
+        Dim Lines() As String = Source.Split(vbLf)
+        Dim Parts() As String
+        Dim Count As Integer = 0
+        Dim ConnCount As Integer = 0
+        Dim Splitter As Char() = {" "c}
+
+        For Count = 0 To Lines.Count - 1
+            'Trim down line.
+            Dim sLine As String = Lines(Count).Trim()
+
+            If Not String.IsNullOrEmpty(sLine) Then
+                If (Count > 3) Then
+                    'Get TCP Connections.
+                    If sLine.StartsWith("TCP") Then
+                        'INC Counter
+                        ReDim Preserve TCPConnections(ConnCount)
+                        'Split line into 4 parts.
+                        Parts = sLine.Split(Splitter, StringSplitOptions.RemoveEmptyEntries)
+
+                        'Store TCP information.
+                        With TCPConnections(ConnCount)
+                            On Error Resume Next
+                            'Local address and port
+                            .LocalAddr = Parts(1).Split(":")(0)
+                            .LocalPort = CInt(Parts(1).Split(":")(1))
+                            'Remote address and port
+                            .RemoteAddr = Parts(2).Split(":")(0)
+                            .RemotePort = CInt(Parts(2).Split(":")(1))
+                            .State = Parts(3)
+                            .PID = CInt(Parts(4))
+                            .Data = CInt(Parts(5))
+                        End With
+
+                        'INC connection counter
+                        ConnCount += 1
+                    End If
+                End If
+            End If
+        Next Count
+
+        'Clear up
+        Erase Lines
+        Erase Parts
+    End Sub
+
+    Private Sub ShowTCPConnections()
+        Dim lvItem As ListViewItem = Nothing
+        Dim iFont As New Font("Verdana", LstItems.Font.Size, FontStyle.Regular)
+        Dim Temp As String = vbNullString
+        Dim bOn As Boolean = False
+
+        For Each Info As TcpInfo In TCPConnections
+            'Used for cell back color.
+            bOn = Not (bOn)
+
+            'Create new list view item.
+            lvItem = New ListViewItem()
+
+            'Change list item back color.
+            If bOn Then
+                lvItem.BackColor = Color.FromArgb(231, 245, 250)
+            Else
+                lvItem.BackColor = Color.White
+            End If
+
+            'Set item font.
+            lvItem.Font = iFont
+            'Set listview image.
+            lvItem.ImageKey = "app.png"
+
+            'Get process appliaction path
+            ' Temp = GetProcessName(Info.PID)
+            Temp = GetPName(Info.PID)
+            If Not String.IsNullOrEmpty(Temp) Then
+                lvItem.Tag = Temp 'Set tag with filename.
+                lvItem.Text = gFilename(Temp)
+                lvItem.SubItems.Add(Info.LocalAddr) 'Local IP
+                lvItem.SubItems.Add(Info.LocalPort) 'Local Port
+                lvItem.SubItems.Add(Info.RemoteAddr) 'Remote IP
+                lvItem.SubItems.Add(Info.RemotePort) 'Remote port
+                lvItem.SubItems.Add(Info.State) 'State
+                lvItem.SubItems.Add(Info.PID) 'PID
+
+                'Add to item listview.
+                LstItems.Items.Add(lvItem)
+            End If
+        Next Info
+
+    End Sub
+
+    Private Sub LstItems_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LstItems.SelectedIndexChanged
+        For Each lvItem As ListViewItem In LstItems.SelectedItems
+            'Get process PID
+            SelectedPID = CLng(lvItem.SubItems(6).Text)
+        Next lvItem
+    End Sub
+
+    Private Sub cmdKill_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdKill.Click
+
+        If SelectedPID.Equals(0) Then
+            SetError("Please select a process to close.")
+        Else
+            If MessageBox.Show("Are you sure you want to close this process",
+                               "Kill Process", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question) = DialogResult.Yes Then
+                'Check if process was closed.
+                If Not CloseProess(SelectedPID) Then
+                    'Opps something went wrong.
+                    SetError("Error closing process.")
+                Else
+                    'Reset process ID and refresh connections.
+                    SelectedPID = 0
+                    RefreshAll()
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub cmdRefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdRefresh.Click
+        'Refresh all connections.
+        RefreshAll()
+    End Sub
+
+    Private Sub cmdClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdClose.Click
+        'Close program.
+        Close()
+    End Sub
+
+    Private Sub frmmain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        RefreshAll()
+       
+
+    End Sub
+End Class
+
+
+Module Tools
+
+    Public Function GetProcessName(ByVal PID As Long) As String
+        Try
+            Return Process.GetProcessById(PID).MainModule.FileName
+        Catch ex As Exception
+            Return "Error Fetch process name"
+        End Try
+    End Function
+    Public Function GetPName(ByVal PID) As String
+        Try
+            Return Process.GetProcessById(PID).ProcessName
+
+        Catch ex As Exception
+            Return "hidden process"
+        End Try
+    End Function
+    Public Function CloseProess(ByVal PID As Long) As Boolean
+        Dim Ret As Boolean = False
+        'Try and close the process.
+
+        Try
+            Process.GetProcessById(PID).Kill()
+            Ret = True
+        Catch ex As Exception
+            Ret = False
+        End Try
+
+        Return Ret
+    End Function
+
+    Public Function GetFilename(ByVal Source As String) As String
+        Dim sPos As Integer = -1
+
+        'Return filename.
+        Try
+            sPos = Source.LastIndexOf("\")
+
+            If sPos <> -1 Then
+                Return Source.Substring(sPos + 1)
+            End If
+
+            Return Source
+        Catch ex As Exception
+            Return Source
+        End Try
+    End Function
+    Public Function gFilename(ByVal pn) As String
+        Try
+            For Each prog As Process In Process.GetProcesses
+                If prog.ProcessName.Contains(pn) Then
+                    Return prog.ProcessName
+                End If
+            Next
+        Catch ex As Exception
+            Return "Filename error"
+        End Try
+    End Function
+    Public Sub SetError(ByVal message As String, Optional ByVal Title As String = "Errror")
+        MessageBox.Show(message, Title, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+    End Sub
+
+    Public Function CaptureDos(ByVal Filename As String, Optional ByVal Parms As String = vbNullString) As String
+        Dim Exec As New System.Diagnostics.Process()
+        Dim Buffer As String = vbNullString
+
+        Try
+            With Exec
+                .StartInfo.RedirectStandardOutput = True
+                .StartInfo.UseShellExecute = False
+                .StartInfo.CreateNoWindow = True
+                .StartInfo.FileName = Filename
+                .StartInfo.Arguments = Parms
+                .Start()
+                'Read in output.
+                Buffer = .StandardOutput.ReadToEnd()
+                'Wait for exit.
+                Exec.WaitForExit()
+                'Return string.
+                Return Buffer
+            End With
+
+        Catch ex As Exception
+            Return vbNullString
+        End Try
+    End Function
+
+End Module
+
